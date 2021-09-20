bb5fd65000e980d0c72d40adc269810aca3942fe
Кирилл Ибрагимов <90390272+fcnby@users.noreply.github.com>
2021-09-20 16:52:18 +0300

    Update run_length_encoding.py

--- run_length_encoding.py
+++ run_length_encoding.py
@@ -1,6 +1,14 @@
 def decode(string):
-    return ''
-
-
-def encode(string):
-    return ''
+    k=1
+    string+=' '
+    snew = ''
+    for i in range(len(string)-1):
+        if string[i] == string[i+1]:
+            k+=1
+        else:
+            if k != 1:
+                snew +=str(k)+string[i]
+            else:
+                snew += string[i]
+            k = 1
+    return snew
